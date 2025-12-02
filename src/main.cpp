8cb31f61df266abf2db549e1847ce5011f2d9a5d
yuvc21 <panduraj06211@gmail.com>
2025-12-02 22:10:01 +0530

    shell building tests' commit

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,11 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
-  // TODO: Uncomment the code below to pass the first stage
   std::cout << "$ ";
+  
+  std::string input;
+  
+  std::getline(std::cin , input);
+  
+  std::cout << input << ": command not found" << std::endl;
 }
