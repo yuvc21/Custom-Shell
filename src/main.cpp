32caceaa38f57a6e83de8f5c5f0398fdeadbcbfe
yuvc21 <panduraj06211@gmail.com>
2025-12-09 17:05:36 +0530

    commit for executingExternalcommands

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <cstdlib>
+#include <cstring>
 #include <filesystem>
 #include <iostream>
 #include <string>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <vector>
 
@@ -24,6 +26,24 @@ std::vector<std::string> split(const std::string &str, char delimeter) {
   return parts;
 }
 
+std::vector<std::string> tokenize(const std::string &input) {
+  std::vector<std::string> tokens;
+  std::string current;
+  for (char c : input) {
+    if (c == ' ' || c == '\t') {
+      if (!current.empty()) {
+        tokens.push_back(current);
+        current.clear();
+      }
+    }else{
+      current += c;
+    }
+  }
+  if(!current.empty()){
+    tokens.push_back(current);
+  }
+  return tokens;
+}
 std::string find_executables_in_path(const std::string &cmd_name) {
   const char *path_cstr = std::getenv("PATH");
   if (path_cstr == nullptr) {
@@ -56,7 +76,7 @@ void handle_type(const std::string &input) {
   if (!text.empty() && text[0] == ' ') {
     text = text.substr(1);
   }
-  
+
   if (text == "echo" || text == "exit" || text == "type") {
     std::cout << text << " is a shell builtin" << std::endl;
   } else {
@@ -68,11 +88,41 @@ void handle_type(const std::string &input) {
     }
   }
 }
+extern char **environ;
+void executeExternal(const std::vector<std::string> &args) {
+  if(args.empty()) return;
+  std::string command_path = find_executables_in_path(args[0]);
+  if (command_path.empty()) {
+    std::cout << args[0] << ": command not found" << std::endl;
+    return;
+  }
+  // fork() creates a child process
+  // returns 0  in child , child's PID in parent, -1 on error
+  pid_t pid = fork();
+  if (pid == -1) {
+    // there was an error in creating a child process
+    std::cerr << "Failed to fork\n";
+    return;
+  }
+  if (pid == 0) {
+    //  === CHILD PROCESS CODE ===
+    std::vector<char *> argv;
+    for (const auto &arg : args) {
+      argv.push_back(const_cast<char *>(arg.c_str()));
+    }
+    // this is a null-terminated array
+    // execve() replaces  current program with the new program
+    argv.push_back(nullptr);
+    execve(command_path.c_str(), argv.data(), environ);
 
-void handle_unknown_command(const std::string &input) {
-  std::cout << input << ": command not found" << std::endl;
+    std::cerr << "Failed to execute " << args[0] << std::endl;
+    exit(1);
+  } else {
+    // === PARENT PROCESS CODE ===
+    int status;
+    waitpid(pid, &status, 0);
+  }
 }
-
 int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
@@ -81,15 +131,16 @@ int main() {
     std::cout << "$ ";
     std::string input;
     std::getline(std::cin, input);
-    
+
     if (input == "exit") {
       break;
     } else if (input.rfind("echo", 0) == 0) {
       handle_echo(input);
     } else if (input.rfind("type", 0) == 0) {
       handle_type(input);
-    } else {
-      handle_unknown_command(input);
-    }
+    }else {
+        auto args = tokenize(input);
+        executeExternal(args);
+      }
   }
 }
